62d6a588ff64066afed166432133feffc0c3b713
shanthipriyaram <>
2021-05-30 21:57:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let cargoHold = ['oxygen tanks', 'space suits', 'parrot', 'instruction manual', 'meal packs', 'slinky', 'security blanket'];
+console.log(cargoHold);
+
+//a) Use bracket notation to replace ‘slinky’ with ‘space tether’. Print the array to confirm the change.
+//cargoHold[5] = "space tether";
+console.log(cargoHold.indexOf('slinky'));
+cargoHold[cargoHold.indexOf('slinky')] = "space tether";
+console.log(cargoHold);
+
+//b) Remove the last item from the array with pop. Print the element removed and the updated array.
+cargoHold.pop();
+
+console.log(cargoHold);
+//c) Remove the first item from the array with shift. Print the element removed and the updated array.
+cargoHold.shift();
+console.log(cargoHold);
+
+//d) Unlike pop and shift, push and unshift require arguments inside the (). Add the items 1138 and ‘20 meters’ to the the array - the number at the start and the string at the end. Print the updated array to confirm the changes.
+cargoHold[0]=1138;
+console.log(cargoHold);
+cargoHold.push("20 meters");
+console.log(cargoHold);
+
+
+//e) Use a template literal to print the final array and its length.
+
+console.log(`The array cargoHold has a length of ${cargoHold.length}.`);
